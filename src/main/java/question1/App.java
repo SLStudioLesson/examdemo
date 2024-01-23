36dafcf3626e263e223476125df21a5ef5e34881
taro <taro.abe@fox-hound.jp>
2024-01-23 18:17:01 +0900

    init

--- src/main/java/question1/App.java
+++ src/main/java/question1/App.java
@@ -7,7 +7,7 @@ public class App {
 
 	public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("aaa何か入力してください: ");
+        System.out.print("何か入力してください: ");
         try {
             String userInput = reader.readLine();
             System.out.println("あなたが入力した内容: " + userInput);
