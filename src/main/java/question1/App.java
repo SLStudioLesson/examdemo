fe17e1fc9043cba2f09bda216df7c69c2c5359ec
Tar0n3 <taro.abe@fox-hound.jp>
2024-01-24 15:53:32 +0900

    revenge

--- src/main/java/question1/App.java
+++ src/main/java/question1/App.java
@@ -6,12 +6,12 @@ import java.io.InputStreamReader;
 public class App {
 
 	public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("何か入力してください: ");
-        try {
-            String userInput = reader.readLine();
-            System.out.println("あなたが入力した内容: " + userInput);
-        } catch (Exception e) {
-        }		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("何か入力してください: ");
+		try {
+			String userInput = reader.readLine();
+			System.out.println("あなたが入力した内容: " + userInput);
+		} catch (Exception e) {
+		}
 	}
 }
--- src/test/java/question1/AppTest.java
+++ src/test/java/question1/AppTest.java
@@ -12,41 +12,41 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
 
-    private final InputStream systemIn = System.in;
-    private final PrintStream systemOut = System.out;
-    private ByteArrayOutputStream testOut;
+	private final InputStream systemIn = System.in;
+	private final PrintStream systemOut = System.out;
+	private ByteArrayOutputStream testOut;
 
-    @BeforeEach
-    public void setUpOutput() {
-        testOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testOut));
-    }
+	@BeforeEach
+	public void setUpOutput() {
+		testOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(testOut));
+	}
 
-    private void provideInput(String data) {
-        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
-        System.setIn(testIn);
-    }
+	private void provideInput(String data) {
+		ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+		System.setIn(testIn);
+	}
 
-    private String getOutput() {
-        return testOut.toString();
-    }
+	private String getOutput() {
+		return testOut.toString();
+	}
 
-    @AfterEach
-    public void restoreSystemInputOutput() {
-        System.setIn(systemIn);
-        System.setOut(systemOut);
-    }
+	@AfterEach
+	public void restoreSystemInputOutput() {
+		System.setIn(systemIn);
+		System.setOut(systemOut);
+	}
 
-    @Test
-    public void testCase1() {
-        String input = "テスト入力";
-        provideInput(input);
+	@Test
+	public void testCase1() {
+		String input = "テスト入力";
+		provideInput(input);
 
-        App.main(new String[0]);
+		App.main(new String[0]);
 
-        String output = getOutput();
-        // AssertJを使った出力の確認
-        Assertions.assertThat(output.trim()).isEqualTo("何か入力してください: あなたが入力した内容: テスト入力");
-    }
+		String output = getOutput();
+
+		Assertions.assertThat(output.trim()).isEqualTo("何か入力してください: あなたが入力した内容: テスト入力");
+	}
 
 }
